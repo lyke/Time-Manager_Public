2666ca4cf6778df5b07abf1801aec3017c6e6d6f
Hugo <hugo.decuq@epitech.eu>
2023-10-24 12:18:21 +0200

    Create Clock context

---
+++ time_manager/lib/time_manager_web/controllers/clock_controller.ex
@@ -0,0 +1,43 @@
+defmodule TimeManagerWeb.ClockController do
+  use TimeManagerWeb, :controller
+
+  alias TimeManager.Clocks
+  alias TimeManager.Clocks.Clock
+
+  action_fallback TimeManagerWeb.FallbackController
+
+  def index(conn, _params) do
+    clocks = Clocks.list_clocks()
+    render(conn, :index, clocks: clocks)
+  end
+
+  def create(conn, %{"clock" => clock_params}) do
+    with {:ok, %Clock{} = clock} <- Clocks.create_clock(clock_params) do
+      conn
+      |> put_status(:created)
+      |> put_resp_header("location", ~p"/api/clocks/#{clock}")
+      |> render(:show, clock: clock)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    clock = Clocks.get_clock!(id)
+    render(conn, :show, clock: clock)
+  end
+
+  def update(conn, %{"id" => id, "clock" => clock_params}) do
+    clock = Clocks.get_clock!(id)
+
+    with {:ok, %Clock{} = clock} <- Clocks.update_clock(clock, clock_params) do
+      render(conn, :show, clock: clock)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    clock = Clocks.get_clock!(id)
+
+    with {:ok, %Clock{}} <- Clocks.delete_clock(clock) do
+      send_resp(conn, :no_content, "")
+    end
+  end
+end
---
+++ time_manager/priv/repo/migrations/20231024101649_create_clocks.exs
@@ -0,0 +1,16 @@
+defmodule TimeManager.Repo.Migrations.CreateClocks do
+  use Ecto.Migration
+
+  def change do
+    create table(:clocks, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :time, :naive_datetime
+      add :status, :boolean, default: false, null: false
+      add :fk_user, references(:users, on_delete: :nothing, type: :binary_id)
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:clocks, [:fk_user])
+  end
+end
