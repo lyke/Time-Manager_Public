4638355f3f083798642c74e4285e1356d37bbec3
Arnaud TROISE <arnaud.troise@epitech.eu>
2023-10-24 12:27:09 +0200

    add docker

---
+++ time_manager/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Docker entrypoint script.
+# Wait until Postgres is ready
+
+mix deps.get
+
+mix ecto.create
+# mix phx.gen.schema User users first_name:string last_name:string
+# mix phx.gen.schema Task tasks title:string description:string status:string user_id:references:users
+
+mix ecto.migrate
+
+mix phx.server
