2947510fe545ffff7ef811f427178027b5956db0
roux <yvan.roux@inrae.fr>
2023-10-25 12:28:30 +0200

    ajout des contraintes non-null sur les champs de clocks

--- time_manager/priv/repo/migrations/20231024101649_create_clocks.exs
+++ time_manager/priv/repo/migrations/20231024101649_create_clocks.exs
@@ -4,9 +4,9 @@ defmodule TimeManager.Repo.Migrations.CreateClocks do
   def change do
     create table(:clocks, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :time, :naive_datetime
+      add :time, :naive_datetime, null: false
       add :status, :boolean, default: false, null: false
-      add :fk_user, references(:users, on_delete: :nothing, type: :binary_id)
+      add :fk_user, references(:users, on_delete: :nothing, type: :binary_id), null: false
 
       timestamps(type: :utc_datetime)
     end
