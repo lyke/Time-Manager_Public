c7c8e60251361e82d7e8517eb397196722739149
roux <yvan.roux@inrae.fr>
2023-10-25 12:27:54 +0200

    ajout des contraintes non-null sur les champs autres que team

--- time_manager/priv/repo/migrations/20231024095713_create_users.exs
+++ time_manager/priv/repo/migrations/20231024095713_create_users.exs
@@ -4,11 +4,11 @@ defmodule TimeManager.Repo.Migrations.CreateUsers do
   def change do
     create table(:users, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :firstname, :string
-      add :lastname, :string
-      add :email, :string
-      add :password, :string
-      add :role, :string
+      add :firstname, :string, null: false
+      add :lastname, :string, null: false
+      add :email, :string, null: false
+      add :password, :string, null: false
+      add :role, :string, null: false
       add :team, :integer
 
       timestamps(type: :utc_datetime)
