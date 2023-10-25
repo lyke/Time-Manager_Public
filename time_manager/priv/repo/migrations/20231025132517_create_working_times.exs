cef7ed124213a076c7e78bb7a710467be4af41a0
roux <yvan.roux@inrae.fr>
2023-10-25 15:59:38 +0200

    ajout contraintes not null

--- time_manager/priv/repo/migrations/20231025132517_create_working_times.exs
+++ time_manager/priv/repo/migrations/20231025132517_create_working_times.exs
@@ -4,9 +4,9 @@ defmodule TimeManager.Repo.Migrations.CreateWorkingTimes do
   def change do
     create table(:working_times, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :start, :naive_datetime
-      add :end, :naive_datetime
-      add :fk_user, references(:users, on_delete: :nothing, type: :binary_id)
+      add :start, :naive_datetime, null: false
+      add :end, :naive_datetime, null: false
+      add :fk_user, references(:users, on_delete: :nothing, type: :binary_id), null: false
 
       timestamps(type: :utc_datetime)
     end
