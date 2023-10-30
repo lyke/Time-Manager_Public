bd16f0dbf33671b370a793b6d45a99af30fa13d7
roux <yvan.roux@inrae.fr>
2023-10-30 17:04:37 +0100

    [user] supression code commenté

--- time_manager/lib/time_manager/teams/team.ex
+++ time_manager/lib/time_manager/teams/team.ex
@@ -9,10 +9,8 @@ defmodule TimeManager.Teams.Team do
 
   schema "teams" do
     field :name, :string
-    # has_many :user_teams, UserTeam
     has_many :user_teams, TimeManager.UserTeams.UserTeam
     many_to_many :users, TimeManager.Accounts.User, join_through: TimeManager.UserTeams.UserTeam
-    # has_many :users, through: [:user_teams, :fk_user]
     timestamps(type: :utc_datetime)
   end
 
