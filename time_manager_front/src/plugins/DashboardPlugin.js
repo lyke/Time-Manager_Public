70c3b5fdf777fe96ad73302edd55405aff53aeb7
Hugo <hugo.decuq@epitech.eu>
2023-11-03 16:40:21 +0100

    Create plugins

---
+++ time_manager_front/src/plugins/DashboardPlugin.js
@@ -0,0 +1,20 @@
+export function disableUpdateUserInputs() {
+    document.getElementsByName("email")[0].disabled = true;
+    document.getElementsByName("firstname")[0].disabled = true;
+    document.getElementsByName("lastname")[0].disabled = true;
+    document.getElementsByName("password")[0].disabled = true;
+    document.getElementById("update-user-button").style.display = "none";
+}
+
+export function enableUpdateUserInputs() {
+    var updateUserButton = document.getElementById("update-user-button").style.display;
+    if (updateUserButton == "initial") {
+        this.hide();
+    } else {
+        document.getElementsByName("email")[0].disabled = false;
+        document.getElementsByName("firstname")[0].disabled = false;
+        document.getElementsByName("lastname")[0].disabled = false;
+        document.getElementsByName("password")[0].disabled = false;
+        document.getElementById("update-user-button").style.display = "initial";
+    }
+}
