1d46763ae0d0ffa420e6fc6db64017e4e767d4a9
Hugo <hugo.decuq@epitech.eu>
2023-11-03 16:40:45 +0100

    Axios base url global config

--- time_manager_front/src/main.js
+++ time_manager_front/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:4000/api';
 
 require('@/assets/main.scss');
 
