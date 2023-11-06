54d4ad60fbc26f0d6fe8ea0ec74bce49d4df6037
kevin ly <kev3010@outlook.fr>
2023-11-06 17:11:16 +0100

    créer fonctions pour filtrer authorisations managers aux users de ses teams

--- time_manager/lib/time_manager_web/controllers/authorization.ex
+++ time_manager/lib/time_manager_web/controllers/authorization.ex
@@ -1,22 +1,28 @@
 defmodule TimeManagerWeb.Authorization do
+  import Ecto.Query, warn: false
+  alias TimeManager.Repo
+  alias TimeManager.Accounts.User
+  use Ecto.Schema
 
-    def verify_user_id(conn, user_id) do
-      header = Enum.find(conn.req_headers, fn {k, _} -> k == "authorization" end)
-      case header do
-        {"authorization", "Bearer " <> token} ->
-        case Phoenix.Token.verify(TimeManagerWeb.Endpoint, "user auth", token, max_age: 86400) do
-          {:ok, token_data} ->
-            if token_data.id == user_id do
-              true
-            else
-              false
-            end
-          {:error, _} -> false
-        end
-      _ ->
-        false
+
+  def verify_user_id(conn, user_id) do
+    header = Enum.find(conn.req_headers, fn {k, _} -> k == "authorization" end)
+    case header do
+      {"authorization", "Bearer " <> token} ->
+      case Phoenix.Token.verify(TimeManagerWeb.Endpoint, "user auth", token, max_age: 86400) do
+        {:ok, token_data} ->
+          if token_data.id == user_id do
+            true
+          else
+            false
+          end
+        {:error, _} -> false
       end
+    _ ->
+      false
+    end
   end
+
 
   def verify_role_manager(conn, manager) do
     header = Enum.find(conn.req_headers, fn {k, _} -> k == "authorization" end)
@@ -53,4 +59,50 @@ defmodule TimeManagerWeb.Authorization do
         false
     end
   end
+
+  def is_same_team(conn, user_id) do
+    case extract_team_from_token(conn) do
+      {:ok, token_team} ->
+        case get_team_from_user_id(user_id) do
+          {:ok, user_team} ->
+            have_common_teams(token_team, user_team)
+          {:error, _} -> false
+        end
+      {:error, _} -> false
+    end
+  end
+
+
+  defp extract_team_from_token(conn) do
+    header = Enum.find(conn.req_headers, fn {k, _} -> k == "authorization" end)
+    case header do
+      {"authorization", "Bearer " <> token} ->
+        case Phoenix.Token.verify(TimeManagerWeb.Endpoint, "user auth", token, max_age: 86400) do
+          {:ok, token_data} ->
+            user_with_teams = Repo.preload(token_data, :teams)
+            teams = user_with_teams.teams
+            {:ok, teams}
+          {:error, _} ->
+            false
+        end
+      _ ->
+        false
+    end
+  end
+
+  defp get_team_from_user_id(user_id) do
+    if user_with_teams = TimeManager.Repo.get(User, user_id) |> TimeManager.Repo.preload(:teams) do
+        teams = user_with_teams.teams
+        {:ok, teams}
+    else
+        {:error, "User not found"}
+    end
+  end
+
+  defp have_common_teams(manager_teams, user_teams) do
+    Enum.any?(manager_teams, fn element ->
+    Enum.member?(user_teams, element)
+    end)
+  end
+
 end
