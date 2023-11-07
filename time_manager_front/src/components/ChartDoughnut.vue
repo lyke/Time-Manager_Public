7effbb13cd3aa3babd4588582ee296b2224c8e63
Hugo <hugo.decuq@epitech.eu>
2023-11-07 09:55:03 +0100

    Fix chart

--- time_manager_front/src/components/ChartDoughnut.vue
+++ time_manager_front/src/components/ChartDoughnut.vue
@@ -3,34 +3,53 @@
 </template>
 
 <script>
+import axios from 'axios';
 import Chart from 'chart.js/auto';
 
 export default {
+    data: function() {
+        return {
+            creditTime: 0,
+            color: "",
+        };
+    },
     methods: { 
         setupChart() {
             const ctx = document.getElementById('myChart');
             const data = {
                 labels: [
-                    'Task 1',
-                    'Task 2',
-                    'Task 3'
+                    'Credit time : ' + this.creditTime,
                 ],
                 datasets: [{
-                    label: 'My First Dataset',
-                    data: [300, 50, 100],
+                    label: 'Time manager chart',
+                    data: [parseInt(this.creditTime)],
                     backgroundColor: [
-                        'rgb(255, 99, 132)',
-                        'rgb(54, 162, 235)',
-                        'rgb(255, 205, 86)'
+                        this.color,
                     ],
                     hoverOffset: 4
                 }]
             };
             new Chart(ctx, { type: 'doughnut', data: data });
+        },
+        getCreditTime() {
+            axios
+                .get("/users/time_credit/" + this.$route.params.id)
+                .then(res => { 
+                    this.creditTime = res.data.time_credit_in_minutes;
+                    this.calculCreditTime(this.creditTime);
+                    this.setupChart();
+                });
+        },
+        calculCreditTime(time) {
+            if (time > 0) {
+                this.color = "rgb(50,205,50)";
+            } else {
+                this.color = "rgb(255, 205, 86)";
+            }
         }
     },
     mounted() {
-        this.setupChart();
+        this.getCreditTime();
     }
 }
 </script>
