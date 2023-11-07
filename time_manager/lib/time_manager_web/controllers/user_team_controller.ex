f83c4cd5027e9c6438ac6f8a02db7a0fd0379275
kevin ly <kev3010@outlook.fr>
2023-11-07 11:24:26 +0100

    athorisations user_team

--- time_manager/lib/time_manager_web/controllers/user_team_controller.ex
+++ time_manager/lib/time_manager_web/controllers/user_team_controller.ex
@@ -8,39 +8,65 @@ defmodule TimeManagerWeb.UserTeamController do
   action_fallback TimeManagerWeb.FallbackController
 
   def index(conn, _params) do
-    user_teams = UserTeams.list_user_teams()
-    render(conn, :index, user_teams: user_teams)
+    if verify_role_super_manager(conn, "super_manager") do
+      user_teams = UserTeams.list_user_teams()
+      render(conn, :index, user_teams: user_teams)
+    else
+      conn
+      |> put_status(:unauthorized)
+      |> json(%{error: gettext("unauthorized")})
+    end
   end
 
   def create(conn, %{"user_team" => user_team_params}) do
-    with {:ok, %UserTeam{} = user_team} <- UserTeams.create_user_team(user_team_params) do
+    if verify_role_super_manager(conn, "super_manager") do
+      with {:ok, %UserTeam{} = user_team} <- UserTeams.create_user_team(user_team_params) do
+        conn
+        |> put_status(:created)
+        |> put_resp_header("location", ~p"/api/user_teams/#{user_team}")
+        |> render(:show, user_team: user_team)
+      end
+    else
       conn
-      |> put_status(:created)
-      |> put_resp_header("location", ~p"/api/user_teams/#{user_team}")
-      |> render(:show, user_team: user_team)
+      |> put_status(:unauthorized)
+      |> json(%{error: gettext("unauthorized")})
     end
   end
 
   def show(conn, %{"id" => id}) do
-    user_team = UserTeams.get_user_team!(id)
-    # user = Accounts.get_user!(id)
-
-    render(conn, :show, user_team: user_team)
+    if verify_role_super_manager(conn, "super_manager") do
+      user_team = UserTeams.get_user_team!(id)
+      render(conn, :show, user_team: user_team)
+    else
+      conn
+      |> put_status(:unauthorized)
+      |> json(%{error: gettext("unauthorized")})
+    end
   end
 
   def update(conn, %{"id" => id, "user_team" => user_team_params}) do
-    user_team = UserTeams.get_user_team!(id)
-
-    with {:ok, %UserTeam{} = user_team} <- UserTeams.update_user_team(user_team, user_team_params) do
-      render(conn, :show, user_team: user_team)
+    if verify_role_super_manager(conn, "super_manager") do
+      user_team = UserTeams.get_user_team!(id)
+      with {:ok, %UserTeam{} = user_team} <- UserTeams.update_user_team(user_team, user_team_params) do
+        render(conn, :show, user_team: user_team)
+      end
+    else
+      conn
+      |> put_status(:unauthorized)
+      |> json(%{error: gettext("unauthorized")})
     end
   end
 
   def delete(conn, %{"id" => id}) do
-    user_team = UserTeams.get_user_team!(id)
-
-    with {:ok, %UserTeam{}} <- UserTeams.delete_user_team(user_team) do
-      send_resp(conn, :no_content, "")
+    if verify_role_super_manager(conn, "super_manager") do
+      user_team = UserTeams.get_user_team!(id)
+      with {:ok, %UserTeam{}} <- UserTeams.delete_user_team(user_team) do
+        send_resp(conn, :no_content, "")
+      end
+    else
+      conn
+      |> put_status(:unauthorized)
+      |> json(%{error: gettext("unauthorized")})
     end
   end
 end
