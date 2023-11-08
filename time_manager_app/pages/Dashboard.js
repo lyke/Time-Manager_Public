718a41974798ed03897fc0f0b8cead77ef754ce6
roux <yvan.roux@inrae.fr>
2023-11-08 16:01:36 +0100

    affichage conditionnelle des onglets de navigations (logged in ou out)

--- time_manager_app/pages/Dashboard.js
+++ time_manager_app/pages/Dashboard.js
@@ -1,6 +1,3 @@
-import {StyleSheet, Text, View} from "react-native";
-import {useContext} from "react";
-import {Context} from "../components/TM_ContextProvider";
 import TM_clock from "../components/TM_clock";
 import TM_container from "../components/TM_container";
 import TM_hourCount from "../components/TM_hourCount";
--- time_manager_app/pages/Main.js
+++ time_manager_app/pages/Main.js
@@ -1,23 +1,37 @@
 // In App.js in a new project
 
 import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import Login from "./Login";
 import Register from "./Register";
 import Dashboard from "./Dashboard";
 import MyTeams from "./MyTeams";
+import {useContext} from "react";
+import {Context} from "../components/TM_ContextProvider";
 
 const Tab = createBottomTabNavigator()
 
 export default function Main() {
+    const {token} = useContext(Context)
+
+    return token === "" ? buildLoggedOutTabs() : buildLoggedInTabs()
+}
+
+function buildLoggedInTabs() {
     return (
         <Tab.Navigator>
-            {}
-            <Tab.Screen name="Login" component={Login} options={{headerShown: false}}/>
-            <Tab.Screen name="Register" component={Register} options={{headerShown: false}}/>
             <Tab.Screen name="Dashboard" component={Dashboard} options={{headerShown: false}}/>
             <Tab.Screen name="My Teams" component={MyTeams} options={{headerShown: false}}/>
         </Tab.Navigator>
+    )
+}
 
-    );
+function buildLoggedOutTabs() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Login" component={Login} options={{headerShown: false}}/>
+            <Tab.Screen name="Register" component={Register} options={{headerShown: false}}/>
+        </Tab.Navigator>
+    )
 }
