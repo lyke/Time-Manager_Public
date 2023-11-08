2d503536ad00e5225df3790cf15900229dd7fb95
roux <yvan.roux@inrae.fr>
2023-11-08 17:28:50 +0100

    structure teams -> un onglet par team

---
+++ time_manager_app/components/Team.js
@@ -0,0 +1,11 @@
+import {Text, View} from "react-native";
+
+export default function Team() {
+
+    return (
+        <View>
+            <Text> aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa </Text>
+            <Text> bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb </Text>
+        </View>
+    )
+}
