c268b2acfd3148c1336ff0d7714de49edc280953
roux <yvan.roux@inrae.fr>
2023-11-09 14:37:09 +0100

    affichage conditionnel de la route "my teams"

--- time_manager_app/pages/Main.js
+++ time_manager_app/pages/Main.js
@@ -8,10 +8,14 @@ import Dashboard from "./Dashboard";
 import TM_MyTeams from "./TM_MyTeams";
 import TM_LogOut from "../components/TM_LogOut";
 import {Ionicons} from "@expo/vector-icons";
+import {useContext} from "react";
+import {Context} from "../components/TM_ContextProvider";
 
 const Tab = createBottomTabNavigator()
 
 export default function Main() {
+    const {user} = useContext(Context)
+
     return (
         <Tab.Navigator
             tabBarOptions={{
@@ -22,7 +26,7 @@ export default function Main() {
             <Tab.Screen name="Login" component={Login} options={{ headerShown: false, tabBarStyle: {display: "none"}, tabBarItemStyle: {display: "none"} }}/>
             <Tab.Screen name="Register" component={Register} options={{ headerShown: false, tabBarStyle: {display:"none"}, tabBarItemStyle: {display: "none"} }}/>
             <Tab.Screen name="Dashboard" component={Dashboard} options={{headerShown: false}}/>
-            <Tab.Screen name="My Teams" component={TM_MyTeams} options={{headerShown: false}}/>
+            {user && user.role === "user" ? <></> : <Tab.Screen name="My Teams" component={TM_MyTeams} options={{headerShown: false}}/> }
             <Tab.Screen name="Logout" component={TM_LogOut} options={{headerShown: false}}/>
         </Tab.Navigator>
     );
